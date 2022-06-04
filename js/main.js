f30c5e4dc74f5b5a420438686fa99d180f24d3da
Bunnasorn Kaewsiri <bunnasorn.k@outlook.com>
2022-06-04 14:00:50 +0700

    Finish signup/login function

--- js/main.js
+++ js/main.js
@@ -3,17 +3,81 @@ const { createApp, ref, computed } = Vue;
 createApp({
   setup() {
     const loggedIn = ref(false);
-    const active = ref("login");
+    const active = ref("signUp");
     const showFood = ref(false);
     let counter = ref(0);
+
+    const username = [
+      "@abcdefg",
+      "@mgh2022",
+      "@deepsleep",
+      "@hckth",
+      "@wantsomesleep",
+      "@sttu123",
+    ];
 
     const User = ref({
       username: "theCosmosTrio",
       gender: "Neutral",
-      dob: "5 June 2022",
+      dob: {
+        day: "5",
+        month: "June",
+        year: "2022",
+      },
       allergies: "Seafood",
       preferences: "Fried Chicken",
     });
+
+    const CreatedUser = ref({
+      username: "",
+      password: "",
+      gender: "",
+      dob: {
+        day: "",
+        month: "",
+        year: "",
+      },
+      allergies: "",
+      preferences: "",
+    });
+
+    const loginForm = ref({
+      username: "",
+      password: "",
+    });
+
+    const signIn = (username, password) => {
+      if (username === CreatedUser.value.username) {
+        if (password === CreatedUser.value.password) {
+          active.value = "foodMatch";
+          loggedIn.value = true;
+          User.value = CreatedUser.value;
+        }
+      }
+    };
+
+    const signOut = () => {
+      CreatedUser.value = {
+        username: "",
+        password: "",
+        gender: "",
+        dob: {
+          day: "",
+          month: "",
+          year: "",
+        },
+        allergies: "",
+        preferences: "",
+      };
+
+      loginForm.value = {
+        username: "",
+        password: "",
+      };
+
+      active.value = "login";
+      loggedIn.value = false;
+    };
 
     const recFood = computed(() => {
       if (counter.value >= recommendFood.length) {
@@ -29,12 +93,16 @@ createApp({
 
     return {
       loggedIn,
+      signIn,
+      signOut,
       active,
       showFood,
       recFood,
       counter,
       recommendFood,
       User,
+      CreatedUser,
+      loginForm,
     };
   },
 }).mount("#app");
