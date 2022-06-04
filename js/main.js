b2dcee6acb9d8aa3ae6d3d452279f1d9655c24dd
Bunnasorn Kaewsiri <bunnasorn.k@outlook.com>
2022-06-04 12:47:46 +0700

    Finish login/signup page

--- js/main.js
+++ js/main.js
@@ -2,10 +2,18 @@ const { createApp, ref, computed } = Vue;
 
 createApp({
   setup() {
-    const loggedIn = ref(true);
-    const active = ref("foodMatch");
-    const showFood = ref(true);
+    const loggedIn = ref(false);
+    const active = ref("login");
+    const showFood = ref(false);
     let counter = ref(0);
+
+    const User = ref({
+      username: "theCosmosTrio",
+      gender: "Neutral",
+      dob: "5 June 2022",
+      allergies: "Seafood",
+      preferences: "Fried Chicken",
+    });
 
     const recFood = computed(() => {
       if (counter.value >= recommendFood.length) {
@@ -26,6 +34,7 @@ createApp({
       recFood,
       counter,
       recommendFood,
+      User,
     };
   },
 }).mount("#app");
---
+++ js/user.js
@@ -0,0 +1,7 @@
+const User = {
+  username: "theCosmosTrio",
+  dob: "5 June 2022",
+  gender: "Neutral",
+  allergies: "Seafood",
+  preferences: "Fried Chicken",
+};
