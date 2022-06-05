955dbe7852850592539a9546719e72e011deff0f
Bunnasorn Kaewsiri <bunnasorn.k@outlook.com>
2022-06-05 07:15:19 +0700

    Remove api

--- js/main.js
+++ js/main.js
@@ -13,30 +13,6 @@ createApp({
     const username = ref("");
     const res = ref("");
     const selectedFood = ref("");
-    const data = ref({});
-
-    const Authorication =
-      "Bearer eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICI1MG9RYTN4NUcyVnJrM0ZScGZnU0lVVjhlQmt0ZzhtZ2RFZmZYX1R6UlE0In0.eyJleHAiOjE2NTQ3OTU1NzIsImlhdCI6MTY1NDM2MzU3MiwianRpIjoiNWRmNjIzYjMtNWNiNy00OWVkLWI5ZjAtNGYxYjFlODE1YjkyIiwiaXNzIjoiaHR0cHM6Ly9pYW0tc3RhZ2luZy5tYWdpY2JveHNvbHV0aW9uLmNvbS9hdXRoL3JlYWxtcy9tYnMiLCJhdWQiOlsicmVhbG0tbWFuYWdlbWVudCIsImFjY291bnQiXSwic3ViIjoiYTRjZGY3NDgtNzJiZi00YzRjLTgwZjgtYTY4ZTE1OTRiZGYxIiwidHlwIjoiQmVhcmVyIiwiYXpwIjoibWFnaWNoYWNrIiwic2Vzc2lvbl9zdGF0ZSI6IjdmOTU3MWE5LTVlZjQtNGUzNi1hZTAyLWFkMTg0MTNlOWFkYSIsImFjciI6IjEiLCJhbGxvd2VkLW9yaWdpbnMiOlsiKiJdLCJyZWFsbV9hY2Nlc3MiOnsicm9sZXMiOlsibWJzX3RlY2huaWNpYW4iLCJvZmZsaW5lX2FjY2VzcyIsImRlZmF1bHQtcm9sZXMtbWJzIiwidW1hX2F1dGhvcml6YXRpb24iXX0sInJlc291cmNlX2FjY2VzcyI6eyJyZWFsbS1tYW5hZ2VtZW50Ijp7InJvbGVzIjpbInZpZXctcmVhbG0iLCJ2aWV3LWlkZW50aXR5LXByb3ZpZGVycyIsIm1hbmFnZS1pZGVudGl0eS1wcm92aWRlcnMiLCJpbXBlcnNvbmF0aW9uIiwicmVhbG0tYWRtaW4iLCJjcmVhdGUtY2xpZW50IiwibWFuYWdlLXVzZXJzIiwicXVlcnktcmVhbG1zIiwidmlldy1hdXRob3JpemF0aW9uIiwicXVlcnktY2xpZW50cyIsInF1ZXJ5LXVzZXJzIiwibWFuYWdlLWV2ZW50cyIsIm1hbmFnZS1yZWFsbSIsInZpZXctZXZlbnRzIiwidmlldy11c2VycyIsInZpZXctY2xpZW50cyIsIm1hbmFnZS1hdXRob3JpemF0aW9uIiwibWFuYWdlLWNsaWVudHMiLCJxdWVyeS1ncm91cHMiXX0sImFjY291bnQiOnsicm9sZXMiOlsibWFuYWdlLWFjY291bnQiLCJtYW5hZ2UtYWNjb3VudC1saW5rcyIsInZpZXctcHJvZmlsZSJdfX0sInNjb3BlIjoicHJvZmlsZSBlbWFpbCB1c2VyX2luZm9ybWF0aW9uIiwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJuYW1lIjoiVmVsdmEgR29sZG5lciIsInByZWZlcnJlZF91c2VybmFtZSI6InZlbHZhODhAbWFpbC5jb20iLCJnaXZlbl9uYW1lIjoiVmVsdmEiLCJmYW1pbHlfbmFtZSI6IkdvbGRuZXIiLCJtYnNDbGllbnRJZCI6ImY2NGJiYTgzLWU5Y2UtNGIyMy04YWRlLTA4Njg3YTFkNDQwNiIsImVtYWlsIjoidmVsdmE4OEBtYWlsLmNvbSJ9.PJU7riWk1HRzLOuvWaLdcXcFiX4misEbFSKcF-wmlk_mZLzcW4O8BxyIIuSadKzQsot6yO0HVQpVBhITLyd3wqUWzTr7WMCp4fUgLnxQI41CAcFSwquxvhm7safy8vUUWexokdQytxjBuFP5-r5fkA1ZSD-Gr6dAZ60o0Qw8XtIeThPWK1BjEOaaQ96AABPPjeI1FqZThLtiQ0Ljgv4kdGfry3pZVGiiQNVNU_e3BIJnzuJIYBqidDWnE6z6y3nsgP8rxm4V1zvqxpTsHmq9ZVoqHc9Xw7dm1OqD7JP__oTaAEXGFG_tLL-9_dzrpIem_1sK4mC_MQuTUmwnqLLruw";
-
-    const getData = async () => {
-      const config = {
-        method: "get",
-        url: "https://api-staging.magicboxsolution.com/address/v1/address/districts?limit=998",
-        headers: {
-          Authorication: Authorication,
-        },
-      };
-      try {
-        const payload = await axios(config);
-
-        console.log(payload.data);
-        data.value = payload;
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
-    getData();
 
     const selectedPrefer = ref([]);
     const preferences = ref({
@@ -192,7 +168,6 @@ createApp({
       filterRecFood,
       showLog,
       selectedFriend,
-      data,
     };
   },
 }).mount("#app");
